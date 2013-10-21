d41f14871495eba56171b86046c6f86b79464477
Joey Lorich <joey@cloudspace.com>
2013-10-21 14:13:54 -0400

    update to auto-skype

--- chrisbot.rb
+++ chrisbot.rb
@@ -4,42 +4,94 @@ require 'googleajax'
 require 'clipboard'
 require 'skype'
 
-# Results the url of the first google search result for the given term
-def get_first_google_result(term)
-  GoogleAjax.referer = "cloudspace.com"
+# module GoogleAjax
+#   class Search < Results
+#     def self.animated(query, latitude, longitude, args = {})
+#       args = { :sll => "#{latitude},#{longitude}" }.merge(args)
+#       get(:local, query, args)
+#     end
+#   end
+# end
 
-  result = GoogleAjax::Search.web(term)[:results][0]
+class ChrisBot
+  attr_accessor :last_question, :chat, :images
 
-  return result[:unescaped_url]
-end
+  def initialize(topic_includes = "cloudspace.com", min_size = 20, images=false)
+    GoogleAjax.referer = "cloudspace.com"
+    self.images = images
 
-# Gets the most recent quesiton out of a skype chat with the given params
-def get_most_recent_skype_question(topic_includes = "cloudspace.com", min_size = 20)
-  chat = Skype.chats.find{|c| c.members.size > min_size and c.topic.include? topic_includes }
+    # Get chat if it's doesn't exist
+    if @chat.nil?
+      @chat = Skype.chats.find{|c| c.members.size > min_size and c.topic.include? topic_includes }
+      puts "Found skype chat"
+    end
 
-  raise 'No skype chat matching those parameters found' if chat.nil?
+    raise 'No skype chat matching those parameters found' if @chat.nil?
+  end
 
-  chat.messages.reverse.each do |m|
-    body = m.body
-    last_char = body.strip.split('').last
-  
-    next if last_char != "?"
+  # Results the url of the first google search result for the given term
+  def get_first_google_result(term)
+    begin
+      if (self.images)
+        result = GoogleAjax::Search.images(term, {imgtype: "animated"})[:results][0]
+      else
+        result = GoogleAjax::Search.web(term)[:results][0]
+      end
+      
+      return nil if result.nil?
+      return result[:unescaped_url]
+    rescue
+      nil
+    end
+  end
 
-    return body
+  # Gets the most recent quesiton out of a skype chat with the given params
+  def get_most_recent_skype_question
+    @chat.messages.reverse.each do |m|
+      body = m.body
+      last_char = body.strip.split('').last
+    
+      next if last_char != "?"
+
+      return m
+    end
+  end
+
+  def answer_last_skype_question
+    # Get the most recent question in the cloudspace.com watercooler
+    question = get_most_recent_skype_question
+
+    # Dont answer if the question hasn't changed
+    return if question.body == @last_question
+    
+    # Log
+    puts question.user + ": " + question.body
+
+    # Get the first search result fo rthat question
+    first_search_result_url = get_first_google_result(question.body)
+
+    if first_search_result_url.nil?
+      puts "  No results found"
+      return
+    end
+
+   puts first_search_result_url
+
+    # Put that question into clipboard
+    @chat.post first_search_result_url
+
+    #Clipboard.copy first_search_result_url
+    @last_question = question.body
+  end
+
+  def self.act_as_chris(topic_includes = "cloudspace.com", min_size = 20, images=true)
+    bot = ChrisBot.new(topic_includes, min_size, true)
+
+    while true do    
+      bot.answer_last_skype_question
+      sleep(1)
+    end
   end
 end
 
-
-def acts_as_chris
-  # Get the most recent question in the cloudspace.com watercooler
-  question = get_most_recent_skype_question
-
-  # Get the first search result fo rthat question
-  first_search_result_url = get_first_google_result(question)
-
-  # Put that question into clipboard
-  Clipboard.copy first_search_result_url
-end
-
-
-acts_as_chris
+ChrisBot.act_as_chris("Bot Test", 0, true)
